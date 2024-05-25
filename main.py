50d99ed15fd6e60a80e09df4f0bf9cd60a68dbd4
master-builder-one <93823957+master-builder-one@users.noreply.github.com>
2024-05-25 16:30:05 +0000

    Updated directories for main.py

--- main.py
+++ main.py
@@ -61,12 +61,12 @@ with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
 
 
 
-file_path = ".\\uct-find-class\\uct-find-class\\src\\data\\"
+file_path = ".\\src\\data\\"
 file_path += "VSchedule.json"
 with open(file_path, "w") as final:
     json.dump(df_array, final)
 
-file = ".\\uct-find-class\\uct-find-class\\src\\data"
+file = ".\\src\\data"
 file += "\heading.txt"
 
 with open(file, "w") as file:
