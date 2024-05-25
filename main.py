9a8af255de4547e7ccff9485adff1a584faa033f
master-builder-one <93823957+master-builder-one@users.noreply.github.com>
2024-05-25 18:49:22 +0000

    Changed main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 import json
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-import os
 
 
 url = "https://ictsapps.uct.ac.za/classroom/"
@@ -13,50 +12,50 @@ soup = BeautifulSoup(response.text, "html5lib")
 
 venues = []
 
+
 for venue in soup.find_all("span", attrs={"class":"menuhead"}):
     outer_tag = venue.parent
     link = outer_tag["href"]
     link = url + link
+    print(venue.text)
     venues.append({"venue": venue.text, "venue_link": link})
 
-df = pd.DataFrame()
 
-def parse_page(a_venue):
-    try: 
-        response = requests.get(a_venue["venue_link"])
-        soup = BeautifulSoup(response.text, "html5lib")
-        table = soup.find("table", attrs = {"id":"tblSchedule"})
+df = pd.DataFrame()
+df_array = []
+i = 0
+file_content = ""
+for a_venue in venues:
+    str_table = ""
+    print(a_venue["venue_link"])
+    response = requests.get(a_venue["venue_link"])
+    soup = BeautifulSoup(response.text, "html5lib")
+    table = soup.find("table", attrs = {"id":"tblSchedule"})
+
+    if(i == 0):
+        file_content = soup.find("h4").text
+    
+
+    if table.text != "":
+        tables_pd = pd.read_html(response.content, index_col=False)
+        tables_pd = tables_pd[0] 
 
         
-       
-
-        if table.text != "":
-            tables_pd = pd.read_html(response.content, index_col=False)
-            tables_pd = tables_pd[0] 
-
-            
-            time_object = {}
-            for i in range(0, 32):
-                time = tables_pd[i].at[0]
-                time = time.split()
-                time_object[time[0]] = ""
-                if len(time) > 1:
-                    time_object[time[0]] = time[1]
+        time_object = {}
+        for i in range(0, 32):
+            time = tables_pd[i].at[0]
+            time = time.split()
+            time_object[time[0]] = ""
+            if len(time) > 1:
+                time_object[time[0]] = time[1]
+            print(time)
 
 
-            time_object["Venue"] = a_venue["venue"]
-            time_object["VenueLink"] = a_venue["venue_link"]
-            return time_object
-        table = None
-    except Exception as e:
-        print('Request failed due to error:', e)
-
-
-MAX_THREADS = min(os.cpu_count(), len(venues))
-df_array = []
-with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-    df_array = list(executor.map(parse_page, venues))
-
+        time_object["Venue"] = a_venue["venue"]
+        time_object["VenueLink"] = a_venue["venue_link"]
+        df_array.append(time_object)
+        i += 1
+    table = None
 
 
 
@@ -69,4 +68,4 @@ with open(file_path, "w") as final:
 
 file_path = os.path.join(base_path, "heading.txt")
 with open(file_path, "w") as file:
-    file.write("date-DD-MM-YYYY")
+    file.write(file_content)
