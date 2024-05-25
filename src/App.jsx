67b9b1aec6a171c5cbed845ff5bdb6e9cc526a6c
master-builder-one <93823957+master-builder-one@users.noreply.github.com>
2024-05-25 16:53:57 +0000

    Removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { createRoot } from 'react-dom/client'
 import './css/App.css'
 import './css/index.css'
 import VenueTable from './components/VenueTable'
